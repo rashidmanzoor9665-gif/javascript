f90f55997735837e4a2c7dc57881947b5d1df8e0
Rashid Manzoor <rashidmanzoor9665@gmail.com>
2025-12-12 21:42:42 +0500

    variables in js

---
+++ variables.js
@@ -0,0 +1,8 @@
+var myName = "56";
+console.log(myName);
+console.log(typeof myName);
+console.log(typeof Number(myName));
+console.log(parseInt("123"));
+console.log(typeof parseInt("123"));
+console.log(parseInt("&1223"));
+console.log(parseInt("-123"));
